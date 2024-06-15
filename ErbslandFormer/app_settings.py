aab187dd54e33d3c102fa7993ae53ce4434233dd
erbsland-dev <github@erbsland.dev>
2024-06-15 19:12:48 +0200

    Increase the application version.

--- ErbslandFormer/app_settings.py
+++ ErbslandFormer/app_settings.py
@@ -14,7 +14,7 @@ from django.contrib.messages import constants as message_constants
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-APP_VERSION = "0.9.3-beta"
+APP_VERSION = "0.9.4-beta"
 
 INSTALLED_APPS = [
     "editor.apps.EditorConfig",
